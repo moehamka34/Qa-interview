ff17dba411c486c03031f70ffdb6026b5a3f0d94
Mohamed Hamka <mohamedhamka@Mohameds-MacBook-Pro-2.local>
2022-07-20 19:31:36 -0400

    Sample complete

--- API.py
+++ API.py
@@ -3,20 +3,20 @@ from collections import OrderedDict
 #urllib is for http requests, for get and post
 #only include request function as that's the only one that we need for this program
 
-#json is to include it as json format
+
 
 with urllib.request.urlopen("https://randomuser.me/api") as url:    # get request for url
     data = json.loads(url.read().decode())                          # present it as JSON
-    key_json = (data['results'][0].keys())                          # display the results
+    key_json = (data['results'][0].keys())                          # save 'names'
 
 
-value = 1
-with open('json_data.json', 'w') as outfile:
-    for key in reversed(key_json):
-        json_dt = {}
-        json_dt['name'] = (data['results'][0][key])
-        json_dt['value'] = (value)
-        json.dump(json_dt,outfile)
-        value = value+1
+value = 1                                                       # initialize 'value'
+with open('json_data.json', 'w') as outfile:                    # open JSON file
+    for key in reversed(key_json):                              # loop through names
+        json_dt = {}                                            # initialize JSON object
+        json_dt['name'] = (data['results'][0][key])   # store value of cur. name in object
+        json_dt['value'] = (value)                             # store current value    
+        json.dump(json_dt,outfile)                             # export object to JSON
+        value = value+1                                     # increase value for next obj
 
 
